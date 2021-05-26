2c8b8a3d55c6b0105658c8bb64d8ae7978e03545
ricardoalonne <71679195+ricardoalonne@users.noreply.github.com>
2021-05-25 19:52:30 -0500

    SIGECA: Gestionar Catálogo (Front al 10%)

---
+++ SIGECA/Controllers/APIS/CatalogoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIGECA.Controllers.APIS
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogoController : ControllerBase
+    {
+        // GET: api/<CatalogoController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CatalogoController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CatalogoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CatalogoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CatalogoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
