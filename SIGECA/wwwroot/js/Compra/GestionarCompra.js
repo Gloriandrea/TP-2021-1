57ffb2603b02d376077871f02d614015e8346b00
Gustavo <gustavopoth@gmail.com>
2021-06-03 15:58:49 -0500

    CUS Gestionar Compras al 100%

--- SIGECA/wwwroot/js/Compra/GestionarCompra.js
+++ SIGECA/wwwroot/js/Compra/GestionarCompra.js
@@ -1,189 +1,60 @@
 ﻿var itemsProductos = [];
 var costoTotal = 0.00;
 $(document).ready(function () {
-
-    $('#tableCompra').DataTable({
-        "language": {
-            "processing": "Procesando...",
-            "lengthMenu": "Mostrar _MENU_ registros",
-            "zeroRecords": "No se encontraron resultados",
-            "emptyTable": "Ningún dato disponible en esta tabla",
-            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-            "search": "Buscar:",
-            "infoThousands": ",",
-            "loadingRecords": "Cargando...",
-            "paginate": {
-                "first": "Primero",
-                "last": "Último",
-                "next": "Siguiente",
-                "previous": "Anterior"
-            },
-            "aria": {
-                "sortAscending": ": Activar para ordenar la columna de manera ascendente",
-                "sortDescending": ": Activar para ordenar la columna de manera descendente"
-            },
-            "buttons": {
-                "copy": "Copiar",
-                "colvis": "Visibilidad",
-                "collection": "Colección",
-                "colvisRestore": "Restaurar visibilidad",
-                "copyKeys": "Presione ctrl o u2318 + C para copiar los datos de la tabla al portapapeles del sistema. <br \/> <br \/> Para cancelar, haga clic en este mensaje o presione escape.",
-                "copySuccess": {
-                    "1": "Copiada 1 fila al portapapeles",
-                    "_": "Copiadas %d fila al portapapeles"
-                },
-                "copyTitle": "Copiar al portapapeles",
-                "csv": "CSV",
-                "excel": "Excel",
-                "pageLength": {
-                    "-1": "Mostrar todas las filas",
-                    "1": "Mostrar 1 fila",
-                    "_": "Mostrar %d filas"
-                },
-                "pdf": "PDF",
-                "print": "Imprimir"
-            },
-            "autoFill": {
-                "cancel": "Cancelar",
-                "fill": "Rellene todas las celdas con <i>%d<\/i>",
-                "fillHorizontal": "Rellenar celdas horizontalmente",
-                "fillVertical": "Rellenar celdas verticalmentemente"
-            },
-            "decimal": ",",
-            "searchBuilder": {
-                "add": "Añadir condición",
-                "button": {
-                    "0": "Constructor de búsqueda",
-                    "_": "Constructor de búsqueda (%d)"
-                },
-                "clearAll": "Borrar todo",
-                "condition": "Condición",
-                "conditions": {
-                    "date": {
-                        "after": "Despues",
-                        "before": "Antes",
-                        "between": "Entre",
-                        "empty": "Vacío",
-                        "equals": "Igual a",
-                        "notBetween": "No entre",
-                        "notEmpty": "No Vacio",
-                        "not": "Diferente de"
-                    },
-                    "number": {
-                        "between": "Entre",
-                        "empty": "Vacio",
-                        "equals": "Igual a",
-                        "gt": "Mayor a",
-                        "gte": "Mayor o igual a",
-                        "lt": "Menor que",
-                        "lte": "Menor o igual que",
-                        "notBetween": "No entre",
-                        "notEmpty": "No vacío",
-                        "not": "Diferente de"
-                    },
-                    "string": {
-                        "contains": "Contiene",
-                        "empty": "Vacío",
-                        "endsWith": "Termina en",
-                        "equals": "Igual a",
-                        "notEmpty": "No Vacio",
-                        "startsWith": "Empieza con",
-                        "not": "Diferente de"
-                    },
-                    "array": {
-                        "not": "Diferente de",
-                        "equals": "Igual",
-                        "empty": "Vacío",
-                        "contains": "Contiene",
-                        "notEmpty": "No Vacío",
-                        "without": "Sin"
-                    }
-                },
-                "data": "Data",
-                "deleteTitle": "Eliminar regla de filtrado",
-                "leftTitle": "Criterios anulados",
-                "logicAnd": "Y",
-                "logicOr": "O",
-                "rightTitle": "Criterios de sangría",
-                "title": {
-                    "0": "Constructor de búsqueda",
-                    "_": "Constructor de búsqueda (%d)"
-                },
-                "value": "Valor"
-            },
-            "searchPanes": {
-                "clearMessage": "Borrar todo",
-                "collapse": {
-                    "0": "Paneles de búsqueda",
-                    "_": "Paneles de búsqueda (%d)"
-                },
-                "count": "{total}",
-                "countFiltered": "{shown} ({total})",
-                "emptyPanes": "Sin paneles de búsqueda",
-                "loadMessage": "Cargando paneles de búsqueda",
-                "title": "Filtros Activos - %d"
-            },
-            "select": {
-                "1": "%d fila seleccionada",
-                "_": "%d filas seleccionadas",
-                "cells": {
-                    "1": "1 celda seleccionada",
-                    "_": "$d celdas seleccionadas"
-                },
-                "columns": {
-                    "1": "1 columna seleccionada",
-                    "_": "%d columnas seleccionadas"
+    
+    $('.datatable-compra').DataTable(
+        {
+            dom: '<"datatable-header"fl><"datatable-scroll"t><"datatable-footer"ip>',
+            "processing": true,
+            "serverSide": true,
+            "searching": true,
+            "sort": true,
+            "lengthChange": false,
+            "autoWidth": false,
+            "ajax": {
+                "url": $('#URL_ComprasListarTodos').val(),
+                "type": "POST",
+                "error": function () {
+                    document.location.reload();
                 }
             },
-            "thousands": ".",
-            "datetime": {
-                "previous": "Anterior",
-                "next": "Proximo",
-                "hours": "Horas",
-                "minutes": "Minutos",
-                "seconds": "Segundos",
-                "unknown": "-",
-                "amPm": [
-                    "am",
-                    "pm"
-                ]
+            "oLanguage": {
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sNext": "Siguiente",
+                    "sPrevious": "Anterior",
+                    "sLast": "Ultimo"
+                },
+                "sInfo": "_START_ a _END_ de _TOTAL_ registros",
+                "sLengthMenu": "Mostrar _MENU_ registros",
+                "sSearch": "Buscar:",
+                "sProcessing": "",
+                "sInfoFiltered": "",
+                "sInfoFiltered": '',
+                "sZeroRecords": "No se encontro registros",
+                "sInfoEmpty": "No hay registros para mostrar"
             },
-            "editor": {
-                "close": "Cerrar",
-                "create": {
-                    "button": "Nuevo",
-                    "title": "Crear Nuevo Registro",
-                    "submit": "Crear"
-                },
-                "edit": {
-                    "button": "Editar",
-                    "title": "Editar Registro",
-                    "submit": "Actualizar"
-                },
-                "remove": {
-                    "button": "Eliminar",
-                    "title": "Eliminar Registro",
-                    "submit": "Eliminar",
-                    "confirm": {
-                        "_": "¿Está seguro que desea eliminar %d filas?",
-                        "1": "¿Está seguro que desea eliminar 1 fila?"
+            "serverParams": function (setting) {
+            },
+            "columns": [
+                { "render": function (data, type, full, meta) { return 'Nacho Meat' } },
+                { "render": function (data, type, full, meta) { return '20112233445' } },
+                { "render": function (data, type, full, meta) { return 'S/. ' + full.costoTotal } },
+                {
+                    "render": function (data, type, full, meta) {
+                        var date = new Date(Date.parse(full.fecha));
+                        return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear()
                     }
                 },
-                "error": {
-                    "system": "Ha ocurrido un error en el sistema (<a target=\"\\\" rel=\"\\ nofollow\" href=\"\\\">Más información&lt;\\\/a&gt;).<\/a>"
-                },
-                "multi": {
-                    "title": "Múltiples Valores",
-                    "info": "Los elementos seleccionados contienen diferentes valores para este registro. Para editar y establecer todos los elementos de este registro con el mismo valor, hacer click o tap aquí, de lo contrario conservarán sus valores individuales.",
-                    "restore": "Deshacer Cambios",
-                    "noMulti": "Este registro puede ser editado individualmente, pero no como parte de un grupo."
+                {
+                    "render": function (data, type, full, meta) {
+                        return '<button class="btn btnVisualizarCompra" style="color: #4AB6B6" data-compra-id="' + full.id + '"><img class="fas fa-eye" /></button>' +
+                            '<button class="btn btnModificarCompra" style="color: #4AB6B6" data-compra-id="' + full.id + '"><img class="fas fa-edit" /></button>' +
+                            '<button class="btn btnCambiarEstadoCompra" style="color: red" data-compra-id="' + full.id + '"><img class="fas fa-ban" /></button>';;
+                    }
                 }
-            },
-            "info": "Mostrando de _START_ a _END_ de _TOTAL_ entradas"
-        }
-    });
-
+            ]
+        });
 });
 
 function addItem() {
@@ -208,6 +79,28 @@ function addItem() {
     itemsProductos.push(item);
 };
 
+function addItemModificar() {
+    var nombre = $('#productoCompraModificar').val();
+    var cantidad = $('#cantidadCompraModificar').val();
+    var unidad = $('#unidadCompraModificar').val();
+    var costo = $('#costoCompraModificar').val();
+    var row = "<tr>" +
+        "<td>" + nombre + "</td>" +
+        "<td>" + cantidad + "</td>" +
+        "<td>" + costo + "</td>" +
+        "</tr>";
+    $('#itemCompraModificar > tbody').append(row);
+
+    costoTotal += parseFloat(costo);
+    var item = new Object();
+    item.nombre = nombre;
+    item.cantidad = parseInt(cantidad);
+    item.unidadMedida = unidad;
+    item.costo = parseFloat(costo);
+
+    itemsProductos.push(item);
+};
+
 function clearDataCompra() {
     $('#productoCompraRegistrar').val('');
     $('#cantidadCompraRegistrar').val('');
@@ -217,9 +110,19 @@ function clearDataCompra() {
     itemsProductos = [];
 }
 
+function clearDataCompraModificar() {
+    $('#productoCompraModificar').val('');
+    $('#cantidadCompraModificar').val('');
+    $('#unidadCompraModificar').val('');
+    $('#costoCompraModificar').val('');
+    $('#itemCompraModificar tbody').empty();
+    itemsProductos = [];
+}
+
 function cerrarModal() {
     clearDataCompra();
     $('#itemCompra tbody').empty();
+    costoTotal = 0.00;
 }
 
 $("#btnRegistrarCompraModal").on("click", function () {
@@ -228,8 +131,6 @@ $("#btnRegistrarCompraModal").on("click", function () {
     Compra.proveedorID = $('#proveedorCompraRegistrar').val();
     Compra.costoTotal = costoTotal;
     Compra.items = itemsProductos;
-
-    console.log(Compra);
 
     $.ajax({
         type: 'post',
@@ -292,6 +193,161 @@ $("#btnRegistrarCompraModal").on("click", function () {
                     '<i class="fa fa-thumbs-down"></i> Volver',
                 confirmButtonAriaLabel: 'Volver',
             });
+        }
+    });
+});
+
+$('#tableCompra').on('click', '.btnModificarCompra', function (e) {
+    clearDataCompraModificar();
+    costoTotal = 0.00;
+    e.preventDefault();
+    var compraID = $(this).attr('data-compra-id');
+    $.ajax({
+        url: $("#URL_ObtenerCompraPorID").val(),
+        type: 'post',
+        data: "idcompra=" + compraID,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+            if (data.result == "success") {
+                var compra = data.value;
+                var itemscompra = compra.items;
+                $("#idCompraModificar").val(compra.id)
+                $("#proveedorCompraModificar").val(compra.proveedorID);
+                itemscompra.forEach(c => {
+                    var row = "<tr>" +
+                        "<td>" + c.nombre + "</td>" +
+                        "<td>" + c.cantidad + "</td>" +
+                        "<td>" + c.costo + "</td>" +
+                        "</tr>";
+                    $('#itemCompraModificar > tbody').append(row);
+
+                    var item = new Object();
+
+                    item.nombre = c.nombre;
+                    item.cantidad = parseInt(c.cantidad);
+                    item.unidadMedida = c.unidadMedida;
+                    item.costo = parseFloat(c.costo);
+                    costoTotal += c.costo;
+
+                    itemsProductos.push(item);
+                });
+                $("#modalModificarCompra").modal('show');
+            }
+            else {
+                console.log("ERROR AL OBTENER LOS DATOS");
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("ERROR AL OBTENER LOS DATOS");
+        }
+    });
+});
+
+$("#btnModificarCompraModal").on("click", function () {
+    var Compra = new Object();
+
+    Compra.id = $('#idCompraModificar').val();
+    Compra.proveedorID = $('#proveedorCompraModificar').val();
+    Compra.costoTotal = costoTotal;
+    Compra.items = itemsProductos;
+
+    $.ajax({
+        url: 'Compra/ActualizarCompra',
+        type: 'post',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(Compra),
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+            if (data.result == "success") {
+                //Escondiendo el Modal
+                $("#modalModificarCompra").modal('hide');
+                //Limpiando los Campos de Texto
+                clearDataCompraModificar();
+                //Recargar Tabla
+                $('.datatable-compra').dataTable().fnDraw();
+                //Mostrando el Mensaje de Exito
+                Swal.fire({
+                    title: '<strong>Listo!</strong>',
+                    icon: 'success',
+                    html:
+                        'Compra Actualizado Satisfactoriamente',
+                    showCloseButton: true,
+                    showCancelButton: false,
+                    focusConfirm: false,
+                    confirmButtonColor: '#3085d6',
+                    confirmButtonText:
+                        '<i class="fa fa-thumbs-up"></i> Continuar',
+                    confirmButtonAriaLabel: 'Continuar',
+                });
+            }
+            else {
+                Swal.fire({
+                    title: '<strong>Error!</strong>',
+                    icon: 'error',
+                    html:
+                        'Lo sentimos, Ocurrió un error Inesperado',
+                    showCloseButton: true,
+                    showCancelButton: false,
+                    focusConfirm: false,
+                    confirmButtonColor: '#d33',
+                    confirmButtonText:
+                        '<i class="fa fa-thumbs-down"></i> Volver',
+                    confirmButtonAriaLabel: 'Volver',
+                });
+            }
+
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+
+            Swal.fire({
+                title: '<strong>Error!</strong>',
+                icon: 'error',
+                html:
+                    'Error del Servidor - Status 500',
+                showCloseButton: true,
+                showCancelButton: false,
+                focusConfirm: false,
+                confirmButtonColor: '#d33',
+                confirmButtonText:
+                    '<i class="fa fa-thumbs-down"></i> Volver',
+                confirmButtonAriaLabel: 'Volver',
+            });
+        }
+    });
+
+});
+
+$('#tableCompra').on('click', '.btnVisualizarCompra', function (e) {
+    $('#itemCompraConsulta tbody').empty();
+    e.preventDefault();
+    var compraID = $(this).attr('data-compra-id');
+    $.ajax({
+        url: $("#URL_ObtenerCompraPorID").val(),
+        type: 'post',
+        data: "idcompra=" + compraID,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+            if (data.result == "success") {
+                var compra = data.value;
+                var itemscompra = compra.items;
+                $("#idCompraConsultar").val(compra.id);
+                $("#proveedorCompraConsultar").val(compra.proveedorID);
+                itemscompra.forEach(c => {
+                    var row = "<tr>" +
+                        "<td>" + c.nombre + "</td>" +
+                        "<td>" + c.cantidad + "</td>" +
+                        "<td>" + c.costo + "</td>" +
+                        "</tr>";
+                    $('#itemCompraConsulta > tbody').append(row);
+                });
+                $("#modalConsultarCompra").modal('show');
+            }
+            else {
+                console.log("ERROR AL OBTENER LOS DATOS");
+            }
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log("ERROR AL OBTENER LOS DATOS");
         }
     });
 });
