6181fb8fd97d2060660703fe87d81dacf1b08d08
josuec4321 <josuec4321@gmail.com>
2021-06-10 14:33:41 -0500

    creacion de la entidad venta

---
+++ SIGECA/Entities/Venta.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIGECA.Entities
+{
+    [BsonDiscriminator(RootClass = true)]
+    [BsonKnownTypes(
+       typeof(VantaPresencial),
+       typeof(VentaOnline))]
+    public class Venta
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+
+        [BsonElement("codigoVenta")]
+        public string codigoVenta { get; set; }
+
+        [BsonElement("tipo")]
+        public string tipo { get; set; }
+
+        [BsonElement("estado")]
+        public string estado { get; set; }
+
+        [BsonElement("items")]
+        public List<Items> items { get; set; } = new List<Items>();
+
+        [BsonElement("total")]
+        public double total { get; set; }
+
+        [BsonElement("fechaVenta")]
+        public DateTime? fechaVenta { get; set; }
+    }
+
+    public class Items { 
+        public string productoID { get; set; }
+        public int cantidad { get; set; }
+        public double subtotal { get; set; }
+    }
+
+    public class VantaPresencial : Venta { 
+        public string usuarioID { get; set; }
+    }
+
+    public class VentaOnline : Venta
+    {
+        public string clienteID { get; set; }
+    }
+}
