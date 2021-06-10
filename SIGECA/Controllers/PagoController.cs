d2d4cfe2b01e9e68d5a1ea31634d33aa2bf3f220
jennyfer agion zambrano lau <woainimasm@gmail.com>
2021-06-10 14:38:54 -0500

    Avance de la interfaz de pago

---
+++ SIGECA/Controllers/PagoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIGECA.Controllers
+{
+    public class PagoController : Controller
+    {
+       public IActionResult Cobro()
+        {
+            return View();
+        }
+    }
+}
---
+++ SIGECA/Services/PagoService.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using SIGECA.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SIGECA.Services
+{
+    public class PagoService
+    {
+        //private readonly IMongoCollection<Venta> _ventas;
+        //public async Task<Venta> GetById(string id)
+        //{
+        //    return await _ventas.FindAsync(new BsonDocument { { "_id", new ObjectId(id) } }).Result.FirstAsync();
+        //}
+    }
+}
