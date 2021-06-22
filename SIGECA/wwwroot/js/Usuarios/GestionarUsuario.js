ed1623a5a9983fe9d222b6ddbc5d56a068c7c2e5
andre ramirez <black56717@gmail.com>
2021-06-22 18:26:16 -0500

    cambiar contraseña sin variables de sesion
    :v:

--- SIGECA/wwwroot/js/Usuarios/GestionarUsuario.js
+++ SIGECA/wwwroot/js/Usuarios/GestionarUsuario.js
@@ -469,6 +469,77 @@ $(function () {
         });
 
     });
+
+    $(function () {
+        function limpiarFormulario() {
+            $("#usuarioContraseña").val('');
+            $("#usuarioContraseñaRepetir").val('');
+        }
+
+        $("#btnCambiarContraseña").on("click", function (e) {
+            var frmCambiarContraseña = $('#frmCambiarContraseña');
+            e.preventDefault();
+            $.ajax({
+                url: frmCambiarContraseña.prop('action'),
+                type: 'post',
+                data: frmCambiarContraseña.serializeArray(),
+                dataType: "json",
+                success: function (data, textStatus, jqXHR) {
+                    if (data.result == "success") {
+                        //Limpiando los Campos de Texto
+                        limpiarFormulario();
+                        //Mostrando el Mensaje de Exito
+                        Swal.fire({
+                            title: '<strong>Listo!</strong>',
+                            icon: 'success',
+                            html:
+                                'Contraseña Cambiada Correctamente',
+                            showCloseButton: true,
+                            showCancelButton: false,
+                            focusConfirm: false,
+                            confirmButtonColor: '#3085d6',
+                            confirmButtonText:
+                                '<i class="fa fa-thumbs-up"></i> Continuar',
+                            confirmButtonAriaLabel: 'Continuar',
+                        });
+                    }
+                    else {
+                        Swal.fire({
+                            title: '<strong>Error!</strong>',
+                            icon: 'error',
+                            html:
+                                'Lo sentimos, Ocurrió un error Inesperado',
+                            showCloseButton: true,
+                            showCancelButton: false,
+                            focusConfirm: false,
+                            confirmButtonColor: '#d33',
+                            confirmButtonText:
+                                '<i class="fa fa-thumbs-down"></i> Volver',
+                            confirmButtonAriaLabel: 'Volver',
+                        });
+                    }
+
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+
+                    Swal.fire({
+                        title: '<strong>Error!</strong>',
+                        icon: 'error',
+                        html:
+                            'Error del Servidor - Status 500',
+                        showCloseButton: true,
+                        showCancelButton: false,
+                        focusConfirm: false,
+                        confirmButtonColor: '#d33',
+                        confirmButtonText:
+                            '<i class="fa fa-thumbs-down"></i> Volver',
+                        confirmButtonAriaLabel: 'Volver',
+                    });
+                }
+            });
+        });
+    });
+
     
     //VISUALIZACION DE USUARIOS
     $('#TableUsuario').on('click', '.btnVisualizarUsuario', function (e) {
