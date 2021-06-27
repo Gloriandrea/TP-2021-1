27c4bf25b42182300710ea202460e664e4184c02
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-06-27 02:31:46 -0500

    vista catalogo 20%

--- SIGECA_Shop/Controllers/CatalogoController.cs
+++ SIGECA_Shop/Controllers/CatalogoController.cs
@@ -7,14 +7,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-
+using SIGECA_Shop.Models;
+using SIGECA_Shop.MTOs;
+using SIGECA_Shop.Services;
 namespace SIGECA_Shop.Controllers
 {
     public class CatalogoController : Controller
     {
+        private readonly CatalogoService _catalogoService;
+        private readonly UsuarioService _clienteService;
+
+        public CatalogoController(CatalogoService catalogoService, UsuarioService clienteService)
+        {
+            _catalogoService = catalogoService;
+            _clienteService = clienteService;
+        }
+
         public async Task<IActionResult> Index()
+        {
+            IEnumerable<CatalogoDTO> productos = await _catalogoService.Get();
+            return View(productos);
+        }
+
+        public async Task<IActionResult> Login()
         {
             return View();
         }
+
+        public async Task<IActionResult> NewClient()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Logear(Cliente usuario)
+        {
+            try
+            {
+
+                var response = await _clienteService.Login(usuario);
+
+                return RedirectToAction("Index", "Home");
+
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\n HTTP  Exception Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return RedirectToAction("Login", "Home");
+            }
+            finally
+            {
+
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
     }
 }
