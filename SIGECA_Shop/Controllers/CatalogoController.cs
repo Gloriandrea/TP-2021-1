a2c7ef595e5ade8e6e0f94a58932f8e5109eefae
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-06-28 00:23:06 -0500

    catalogo modal

--- SIGECA_Shop/Controllers/CatalogoController.cs
+++ SIGECA_Shop/Controllers/CatalogoController.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using SIGECA_Shop.Models;
-using SIGECA_Shop.MTOs;
 using SIGECA_Shop.Services;
 namespace SIGECA_Shop.Controllers
 {
@@ -28,43 +27,5 @@ namespace SIGECA_Shop.Controllers
             IEnumerable<CatalogoDTO> productos = await _catalogoService.Get();
             return View(productos);
         }
-
-        public async Task<IActionResult> Login()
-        {
-            return View();
-        }
-
-        public async Task<IActionResult> NewClient()
-        {
-            return View();
-        }
-
-        public async Task<IActionResult> Logear(Cliente usuario)
-        {
-            try
-            {
-
-                var response = await _clienteService.Login(usuario);
-
-                return RedirectToAction("Index", "Home");
-
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\n HTTP  Exception Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-                return RedirectToAction("Login", "Home");
-            }
-            finally
-            {
-
-            }
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
     }
 }
