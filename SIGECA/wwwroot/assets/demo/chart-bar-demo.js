da47136f7392e65d9e0316d41b2edd7ec713e1bf
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-07-27 00:30:13 -0500

    reportes

--- SIGECA/wwwroot/assets/demo/chart-bar-demo.js
+++ SIGECA/wwwroot/assets/demo/chart-bar-demo.js
@@ -1,25 +1,25 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#292b2c';
+//Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+//Chart.defaults.global.defaultFontColor = '#292b2c';
 
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["pecho", "Alas", "Pescueso", "Viceras de pollo", "Muslo de pollos frescos", "Pechugas fritas emanizadas"],
     datasets: [{
-      label: "Revenue",
+      label: "Unidades",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [60, 506, 301, 303, 106, 100],
     }],
   },
   options: {
     scales: {
       xAxes: [{
         time: {
-          unit: 'month'
+          unit: 'product'
         },
         gridLines: {
           display: false
@@ -31,7 +31,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 1000,
           maxTicksLimit: 5
         },
         gridLines: {
